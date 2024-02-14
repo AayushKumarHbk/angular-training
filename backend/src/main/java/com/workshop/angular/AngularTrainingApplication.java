3e4bd07a85375a7b1a9c6e9e9ba8d59a9a5bd292
G516309 <aayush.kumar@idemia.com>
2024-02-14 02:07:20 +0100

    Fix cors issue

--- backend/src/main/java/com/workshop/angular/AngularTrainingApplication.java
+++ backend/src/main/java/com/workshop/angular/AngularTrainingApplication.java
@@ -1,15 +1,31 @@
 package com.workshop.angular;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.workshop.angular.repository")
 public class AngularTrainingApplication {
 
+	@Value("${allowed.origin}")
+	private String allowedOrigin;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AngularTrainingApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(allowedOrigin);
+			}
+		};
+	}
 }
